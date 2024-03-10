b35e8bde4cfd9ae06ba8670e89b867b807380eb0
Ajay <ajay.singh.114085@gmail.com>
2024-03-11 00:55:13 +0530

    If else Introduction section

--- Problem1.java
+++ Problem1.java
@@ -17,6 +17,19 @@ public class Problem1 {
             int N = scanner.nextInt();
             scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
+            if(N%2!=0){
+                System.out.println("Weird");
+            }
+            else if(N>=2&&N<=5){
+                System.out.println("Not Weird");
+            }
+            else if(N>=6&&N<=20){
+                System.out.println("Weird");
+            }
+            else if(N>20){
+                System.out.println("Not Weird");
+            }
+
             scanner.close();
         }
     }
