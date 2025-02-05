6bbc650a51390b40ede9891a2a3696aeb35eb469
Ajay <ajay.singh.114085@gmail.com>
2025-02-05 23:18:58 +0530

    Added QuickSort

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -8,7 +8,7 @@ public class BubbleSort {
     public static void bubbleSort(int nums[]){
         int n = nums.length;
         boolean swapped;
-
+        int iteration=0;
         for(int i=0; i<n-1; i++){
             swapped=false;
             for(int j=0;j<n-i-1;j++){
@@ -17,15 +17,18 @@ public class BubbleSort {
                     int temp=nums[j];
                     nums[j]=nums[j+1];
                     nums[j+1]=temp;
+                    swapped=true;
                 }
-
-                if (swapped)break;
-            }
+            iteration++;
+                System.out.println("iteration:"+iteration);
+            }if (!swapped){
+System.out.println("Iteration:"+iteration);
+            }break;
         }
 
     }
     public static void main(String[] args) {
-        int[] arr = {5, 1, 4, 2, 8};
+        int[] arr = {1, 4, 2, 5,8};
         bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
---
+++ sorting/QuickSort.java
@@ -0,0 +1,43 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class QuickSort {
+
+    public static int partition(int nums[],int low, int high){
+
+        int pivot= nums[high];
+        int i=low-1;
+        for(int j=low;j<high;j++){
+            if(nums[j]<pivot) {
+                //swap
+                i++;
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        i++;
+        int temp = nums[i];
+        nums[i]=pivot;
+        nums[high]=temp;
+
+    return i;
+    }
+
+
+    public static void quickSort(int nums[],int low,int high){
+        if(low<high){
+            int pidx= partition(nums,low,high);
+          //  System.out.println("pidx :"+pidx);
+            quickSort(nums,low,pidx-1);
+            quickSort(nums,pidx+1,high);
+        }
+
+    }
+    public static void main(String[] args) {
+        int nums[]={6,3,9,5,2,8};
+        quickSort(nums,0,nums.length-1);
+        System.out.println(" "+Arrays.toString(nums));
+    }
+}
