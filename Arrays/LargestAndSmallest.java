621c03d17effbf495fdb69e8ab9362c0d3adbb41
Ajay <ajay.singh.114085@gmail.com>
2025-03-09 23:10:39 +0530

    Added Min Max Fixed

--- Arrays/LargestAndSmallest.java
+++ Arrays/LargestAndSmallest.java
@@ -5,7 +5,7 @@ public class LargestAndSmallest {
 
     public static int[] minMax(int arr[]){
         int max=0,min=0;
-        for(int i=0;i<arr.length-2;i++){
+        for(int i=0;i<arr.length-1;i++){
             if(arr[min]>arr[i+1]){
                 min=i+1;
             }
@@ -16,7 +16,7 @@ public class LargestAndSmallest {
         return new int[]{arr[min],arr[max]};
     }
     public static void main(String[] args) {
-    int arr[] ={3, 1, 5, 9, 2, 8};
+    int arr[] ={3, 1, 5,9, 2, 8,11};
 
     int result [] = minMax(arr);
     for(int i:result){
