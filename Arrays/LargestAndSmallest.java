8f8ea9f2886b730788dc1cc8628b03afa36b9cc4
Ajay <ajay.singh.114085@gmail.com>
2025-03-09 18:54:18 +0530

    Added Min Max Order of N Solution.

---
+++ Arrays/LargestAndSmallest.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+public class LargestAndSmallest {
+
+
+    public static int[] minMax(int arr[]){
+        int max=0,min=0;
+        for(int i=0;i<arr.length-2;i++){
+            if(arr[min]>arr[i+1]){
+                min=i+1;
+            }
+            if(arr[max]<arr[i+1]){
+                max=i+1;
+            }
+        }
+        return new int[]{arr[min],arr[max]};
+    }
+    public static void main(String[] args) {
+    int arr[] ={3, 1, 5, 9, 2, 8};
+
+    int result [] = minMax(arr);
+    for(int i:result){
+        System.out.print(STR."\{i} ");
+    }
+    }
+}
