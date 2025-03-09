ac3fffc8f541e1a4f7a1bf70a86a98040fc639d5
Ajay <ajay.singh.114085@gmail.com>
2025-03-09 14:10:33 +0530

    Added QuickSort

--- sorting/QuickSort.java
+++ sorting/QuickSort.java
@@ -4,40 +4,40 @@ import java.util.Arrays;
 
 public class QuickSort {
 
-    public static int partition(int nums[],int low, int high){
+    public static int partition(int numbs[],int low, int high){
 
-        int pivot= nums[high];
+        int pivot= numbs[high];
         int i=low-1;
         for(int j=low;j<high;j++){
-            if(nums[j]<pivot) {
+            if(numbs[j]<pivot) {
                 //swap
                 i++;
-                int temp = nums[j];
-                nums[j] = nums[i];
-                nums[i] = temp;
+                int temp = numbs[j];
+                numbs[j] = numbs[i];
+                numbs[i] = temp;
             }
         }
         i++;
-        int temp = nums[i];
-        nums[i]=pivot;
-        nums[high]=temp;
+        int temp = numbs[i];
+        numbs[i]=pivot;
+        numbs[high]=temp;
 
     return i;
     }
 
 
-    public static void quickSort(int nums[],int low,int high){
+    public static void quickSort(int numbs[],int low,int high){
         if(low<high){
-            int pidx= partition(nums,low,high);
-          //  System.out.println("pidx :"+pidx);
-            quickSort(nums,low,pidx-1);
-            quickSort(nums,pidx+1,high);
+            int partitioned= partition(numbs,low,high);
+          //  System.out.println("partitioned :"+partitioned);
+            quickSort(numbs,low,partitioned-1);
+            quickSort(numbs,partitioned+1,high);
         }
 
     }
     public static void main(String[] args) {
-        int nums[]={6,3,9,5,2,8};
-      //  quickSort(nums,0,nums.length-1);
-        System.out.println(Arrays.toString(nums));
+        int numbs[]={6,3,9,5,2,8};
+      //  quickSort(numbs,0,numbs.length-1);
+        System.out.println(Arrays.toString(numbs));
     }
 }
