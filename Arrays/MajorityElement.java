01c2c71bda4571edff81501835346dadb657179c
Ajay <ajay.singh.114085@gmail.com>
2025-03-11 09:18:08 +0530

    Added MajorityElement inside an array without using extra space with a runtime o(n) and space o(1)

---
+++ Arrays/MajorityElement.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class MajorityElement {
+    public static int majorityElement(int[] nums) {
+        int result=nums[0];
+        int count=0;
+        for(int i: nums){
+            if(count==0){
+                result=i;
+            }
+            count +=(i==result)?1:-1;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int nums[]=new int[]{2,2,1,1,1,2,2};
+        System.out.println(majorityElement(nums));
+    }
+
+}
