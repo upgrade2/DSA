014a44fc47b0e69a488582a4aeca208f3091df9e
Ajay <ajay.singh.114085@gmail.com>
2025-03-11 09:21:48 +0530

    Added Move Zeros with runtime complexity is o(n)

---
+++ Arrays/MoveZeros.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class MoveZeros {
+    public static void moveZeroes(int[] nums) {
+        int left =0;
+        for(int right=0;right<nums.length;right++){
+            if(nums.length>1){
+                if(nums[right]!=0){
+                    int temp = nums[right];
+                    nums[right]=nums[left];
+                    nums[left]=temp;
+                    left++;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int nums[] = new int[]{0,1,0,3,12};
+        moveZeroes(nums);
+        for(int i : nums){
+            System.out.print(i+" ");
+        }
+    }
+}
