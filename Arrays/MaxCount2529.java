f489a9ea7a610137c42c7fe565b271dd8493b5e7
Ajay <ajay.singh.114085@gmail.com>
2025-03-13 01:32:21 +0530

    Added MAX Count

---
+++ Arrays/MaxCount2529.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class MaxCount2529 {
+    public static int maximumCount(int[] nums) {
+        int pCount=0,nCount=0;
+        for(int i:nums){
+            if(i<0){pCount++;}
+            if(i>0){nCount++;}
+        }
+       if(pCount>nCount){
+           return pCount;
+       }
+       else return nCount;
+    }
+
+    public static void main(String[] args) {
+        int nums[]={-2,-1,-1,1,2,3,3,3-1};
+        System.out.print(maximumCount(nums));
+
+    }
+}
