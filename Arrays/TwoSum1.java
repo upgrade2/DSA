3ad578743da25d30631616bdf1c156b67462504e
Ajay <ajay.singh.114085@gmail.com>
2025-03-21 21:54:39 +0530

    Added Leetcode 1 best approach

---
+++ Arrays/TwoSum1.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+import java.util.HashMap;
+
+//Using a HashMap and unsorted array
+//o(n) and o(1)
+public class TwoSum1 {
+    public static int[] twoSum(int nums[],int target){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int complement = target-nums[i];
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement),i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[2];
+    }
+
+    public static void main(String[] args) {
+        int nums[]={2,7,11,15};
+        int target=9;
+
+        for(int i : twoSum(nums,target)){
+            System.out.print(i+" ");
+        }
+
+    }
+}
