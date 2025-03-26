90d1167f0315297e7884e3f8364182827f1c1855
Ajay <ajay.singh.114085@gmail.com>
2025-03-27 00:43:36 +0530

    Added Better Approach for Leetcode MergeSortedArrays_88

--- twoPointers/MergeSortedArrays_88.java
+++ twoPointers/MergeSortedArrays_88.java
@@ -2,14 +2,28 @@ package twoPointers;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 
 public class MergeSortedArrays_88 {
     public  void merge(int[] nums1, int m, int[] nums2, int n) {
 
-       for(int i=0;i<n;i++){
-           nums1[m+i]=nums2[i];
-       }
-       Arrays.sort(nums1);
+        int last = m+n-1;
+
+        while(m>0&&n>0){
+            if(nums1[m-1]>nums2[n-1]){
+                nums1[last] = nums1[m-1];
+                m--;
+            }
+            else {
+                nums1[last] = nums2[n-1];
+                n--;
+            }
+            last--;
+        }
+        while (n>0)
+            nums1[last]=nums2[n-1];
+        n--;
+        last--;
     }
     public static void main(String[] args) {
         MergeSortedArrays_88 ms = new MergeSortedArrays_88();
