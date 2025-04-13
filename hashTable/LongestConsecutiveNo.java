6d0576ab42559a6c2addd208ef8004e31bcf4f8d
Ajay <ajay.singh.114085@gmail.com>
2025-04-13 09:47:09 +0530

    Leetcode Solved : Better Solution.

--- hashTable/LongestConsecutiveNo.java
+++ hashTable/LongestConsecutiveNo.java
@@ -1,30 +1,42 @@
 package hashTable;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 public class LongestConsecutiveNo {
 
     public static int longestConsecutive(int[] nums) {
-
         if(nums.length==0)return 0;
-        int[] sortedArray= Arrays.stream(nums).sorted().toArray();
-        int result = 0,count=0;
+        int longestLength=0;
+        Map<Integer,Boolean> map = new HashMap<>();
 
-        int i=1;
-        while(i<sortedArray.length){
-            if(sortedArray[i]-sortedArray[i-1]==1){
-                count++;
-                result = Math.max(count,result);
-            }
-            else if (sortedArray[i-1] == sortedArray[i]) {
-
-            }
-            else count=0;
-            i++;
+        for(int num:nums){
+            map.put(num,Boolean.FALSE);
         }
 
-        return result+1;
+        for (int num:nums){
+            int currentLength=1;
+            int next = num+1;
+            //Forwar direction
+        while(map.containsKey(next)&&!map.get(next)){
+            currentLength++;
+            map.put(next,Boolean.TRUE);
+            next++;
+        }
+
+            int prev = num-1;
+            //Backword Direction
+        while(map.containsKey(prev)&&!map.get(prev)){
+            currentLength++;
+            map.put(prev,Boolean.TRUE);
+            prev--;
+        }
+        longestLength = Math.max(currentLength,longestLength);
+        }
+
+
+        return longestLength;
     }
     public static void main(String[] args) {
         int[] nums = {1,0,1,2};//{0,3,7,2,5,8,4,6,0,1};//{100,4,200,1,3,2};
