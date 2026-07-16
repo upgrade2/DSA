1b07a0d68088fe2db45bde2dbcd7234b3be29659
Ajay <ajay.singh.114085@gmail.com>
2026-07-17 00:44:06 +0530

    Daily Temperatures DSA

---
+++ stack/DailyTemperatures.java
@@ -0,0 +1,31 @@
+package stack;
+
+import java.util.Stack;
+
+public class DailyTemperatures {
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        Stack<int[]> stack = new Stack<>();
+
+        for(int i=0;i<temperatures.length;i++){
+            int t = temperatures[i];
+            while(!stack.empty() && t>stack.peek()[0]){
+            int[] pair = stack.pop();
+            result[pair[1]] = i-pair[1];
+            }
+            stack.push(new int[]{t,i});
+        }
+
+        return result;
+    }
+    public static void main(String[] args) {
+    int[] input = {30,38,30,36,35,40,28};
+    int[] result = new DailyTemperatures().dailyTemperatures(input);
+    for(int i:result)
+    System.out.print(i+" ");
+    }
+
+//    1,4,1,2,1,0,0 output
+
+}
